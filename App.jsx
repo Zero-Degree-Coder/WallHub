f9ad607abd6b796165dcc97d29aeabab66a4de9e
mdalishanali <md.alishanali88@gmail.com>
2024-03-31 22:24:44 +0530

    project init

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
